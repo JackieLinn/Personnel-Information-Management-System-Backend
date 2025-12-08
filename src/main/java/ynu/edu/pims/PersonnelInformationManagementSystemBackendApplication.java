4bd76f9c2b6f58841f266451cd7005ed10559705
JackieLinn <linxy20040304@163.com>
2025-12-08 22:41:19 +0800

    First commit

---
+++ src/main/java/ynu/edu/pims/PersonnelInformationManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ynu.edu.pims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelInformationManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelInformationManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ynu/edu/pims/PersonnelInformationManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ynu.edu.pims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelInformationManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
